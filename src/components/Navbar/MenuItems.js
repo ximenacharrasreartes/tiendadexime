da28ec0389c359c723cb9b875fee4c88b5b5ad90
tiendadexime <ximena.charras@vtex.com.br>
2021-10-12 23:27:53 -0300

    navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,38 @@
+export const MenuItems = [
+    {
+        title: 'Tienda de Xime',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Capsulas',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Máquinas de Café',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Accesorios',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sobre Nosotros',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+    {
+        title: 'Locales',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Registrase',
+        url: '#',
+        cName:'nav-links'
+    }
+
+]
