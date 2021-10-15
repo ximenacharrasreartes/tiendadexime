0d750bcb8db65a0a1ab4af6e4e423fb62828cbc2
tiendadexime <ximena.charras@vtex.com.br>
2021-10-15 01:54:31 -0300

    Cart15

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -16,11 +16,27 @@ const Cart = () => {
     } = useCart();
     if (isEmpty) return <h1 className= "text-center">Aún no agregaste nada a tu carrito. Agregá tus favoritos! </h1>
     return (
-        <section className="py-4 container">
+        <section className="py-4 container"> Tu carrito
 
         <div className="row justify-content-center">
             <div className="col-12">
-            <h5> Tu Carrito ({totalUniqueItems}) total Items: ({}) </h5>
+            <h5> Tus productos ({totalUniqueItems}) Total unidades: ({totalItems}) </h5>
+            <table className="table table-light table-hover m-0">
+                    <tbody> 
+                        {items.map((item, index)=>{
+                            return(
+                             <tr key={index}>
+                                 <td>
+                                    <img src={item.img} style={{height: '6rem'}}/>
+                                 </td>
+                                <td> {item.title}</td>
+                                <td>{item.price}</td>
+                                <td> Cantidad ({item.quantity})</td>
+                            </tr>
+                            )
+                })}
+                    </tbody>
+             </table>
             </div>
         </div>
         </section>
