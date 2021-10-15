02adba9587d3fac7a42882d4c8544369fb5a8fbb
tiendadexime <ximena.charras@vtex.com.br>
2021-10-15 02:09:27 -0300

    Cart13

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -32,6 +32,13 @@ const Cart = () => {
                                 <td> {item.title}</td>
                                 <td>{item.price}</td>
                                 <td> Cantidad ({item.quantity})</td>
+                                <td>
+                                    <button className="btn btn-info m-2" onClick= {() => updateItemQuantity (item.id, item.quantity -1)}> Restar</button>
+                                    <button className="btn btn-info m-2" onClick= {() => updateItemQuantity (item.id, item.quantity +1)}> Agregar</button>
+                                    <button className="btn btn-danger m-2" onClick= {() => removeItem (item.id)}> Eliminar Producto</button>
+
+
+                                </td>
                             </tr>
                             )
                 })}
