32099ea0865d77ac5a45687b711b5ee2d6fb5c61
tiendadexime <ximena.charras@vtex.com.br>
2021-10-14 23:48:28 -0300

    cartWidget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
 
 function App() {
     return ( <div className = "App">
         <Navbar/>
-        
+        <Home />
+    
         </div>
     );
 }
