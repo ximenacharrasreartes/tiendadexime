aad4a158b1ca3102ef8c853cc1a780b6d2aaf5ae
tiendadexime <ximena.charras@vtex.com.br>
2021-10-15 01:40:48 -0300

    Carrito

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,28 @@ import React from 'react';
 import {useCart} from "react-use-cart";
 
 const Cart = () => {
-    const {items } = useCart();
+    const {
+        isEmpty,
+        totalUniqueItems,
+        items,
+
+        totalItems,
+        cartTotal,
+        updateItemQuantity,
+        removeItem,
+        emptyCart,
+
+    } = useCart();
+    if (isEmpty) return <h1 className= "text-center">Aún no agregaste nada a tu carrito. Agregá tus favoritos! </h1>
     return (
-        <div>
-            {console.warn(items)}
-            Tu Carrito
+        <section className="py-4 container">
+
+        <div className="row justify-content-center">
+            <div className="col-12">
+            <h5> Tu Carrito ({totalUniqueItems}) total Items: ({}) </h5>
+            </div>
         </div>
+        </section>
     );
 };
 
