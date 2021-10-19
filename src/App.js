65d2c8a5eac1f01baedafa3071b5e2dc371f1bc2
tiendadexime <ximena.charras@vtex.com.br>
2021-10-19 20:51:59 -0300

    itemcount

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
-import Cart from './components/Cart/Cart'
+import Cart from './components/ItemCount/ItemCount'
 import {CartProvider} from "react-use-cart";
 
 function App() {
--- src/components/Button.js
+++ src/components/Button.js
@@ -18,10 +18,6 @@ export const Button = ({
     buttonStyle,
     buttonSize,
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
-
     return (
         <button className={'btn ${checkButtonStyle} ${checkButtonSize}'} onClick={onClick}
     type={type}>
