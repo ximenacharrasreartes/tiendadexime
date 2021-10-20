82bf18690074b56f038a053b7c5ad4d9afb93f49
tiendadexime <ximena.charras@vtex.com.br>
2021-10-20 00:50:51 -0300

    ItemCount

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -14,15 +14,16 @@ class ItemCount extends Component {
     increment = () => {
         this.setState({ count: this.state.count + 1});
     }
-    increment = () => {
+    decrement = () => {
         this.setState({ count: this.state.count - 1});
     }
 
     render() {
         return (
             <div className="ItemCount">
-                <button class="btn btn-dark" onClick={this.increment} className="counter"> + </button>
-                <button class="btn btn-dark" className="counter">-</button>
+                <button class="btn btn-dark" onClick={this.increment} > + </button>
+                <button class="btn btn-dark" onClick={this.decrement}>-</button>
+                <button class="btn btn-dark" className> Restar</button>
                 <h2>{this.state.count}</h2>
 
 
